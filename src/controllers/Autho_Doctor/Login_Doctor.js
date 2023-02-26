837f1fa35d0d88484a5d5768b427dc6ac42c9d13
Ariel-Alegre <arielalegre98@gmail.com>
2023-02-25 22:47:41 -0300

    first commit

--- src/controllers/Autho_Doctor/Login_Doctor.js
+++ src/controllers/Autho_Doctor/Login_Doctor.js
@@ -25,7 +25,7 @@ module.exports = {
             }
 
         } catch (error) {
-            console.log(
+            res.send(
                 `[Login Users Doctor] - [Login_Doctor - POST]: ${error}`
             )
         }
